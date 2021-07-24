993ed20be164270e651bce62969d7bbf6136a759
valerenegoh <valerenegoh@gmail.com>
2021-07-24 15:09:27 +0800

    Initial Commit

---
+++ Shared/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Shared
+//
+//  Created by Valerene Goh Ze Yi on 24/7/21.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
