8ef4c4ce7930ffc7bc4fa8a6b42f40eb4e8b42de
valerenegoh <valerenegoh@gmail.com>
2021-07-25 13:47:09 +0800

    [Valerene] business card implementation

---
+++ Shared/InfoView.swift
@@ -0,0 +1,20 @@
+//
+//  InfoView.swift
+//  Demo
+//
+//  Created by Valerene Goh Ze Yi on 24/7/21.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView()
+    }
+}
